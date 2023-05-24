7fb64da0780ac20c86571b4010615b5ff424aa03
Esteban Navarro <esteban.navarro.orellana@gmail.com>
2023-05-24 15:37:52 -0500

    Feature: Configurar arreglo de sitios javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,60 @@
-let profileName = document.querySelector(".profile__name");
-let profileSubtitle = document.querySelector(".profile__subtitle");
-let profileButtonEdit = document.querySelector(".profile__button-edit");
+const profileName = document.querySelector(".profile__name");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const profileButtonEdit = document.querySelector(".profile__button-edit");
 
-let popUp = document.querySelector(".popup");
-let popUpButtonClose = document.querySelector(".popup__button-close");
+const popUp = document.querySelector(".popup");
+const popUpButtonClose = document.querySelector(".popup__button-close");
 
-let popUpForm = document.querySelector(".popup__form");
-let popUpName = document.querySelector(".popup__input_name");
-let popUpAboutUs = document.querySelector(".popup__input_about-us");
-let popUpButtonSave = document.querySelector(".popup__button-save");
+const popUpForm = document.querySelector(".popup__form");
+const popUpName = document.querySelector(".popup__input_name");
+const popUpAboutUs = document.querySelector(".popup__input_about-us");
+const popUpButtonSave = document.querySelector(".popup__button-save");
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+initialCards.forEach((element) => {
+  addElement(element.name, element.link);
+});
+
+function addElement(name, link) {
+  const elements = document.querySelector(".elements");
+  const elementTemplate = document.querySelector("#element-template").content;
+  const elementItem = elementTemplate.querySelector(".element").cloneNode(true);
+  const elementImage = elementItem.querySelector(".element__image");
+
+  elementItem.querySelector(".element__title").textContent = name;
+
+  elementItem
+    .querySelector(".element__image")
+    .setAttribute("style", "background-image: url(" + link + ")");
+
+  elements.append(elementItem);
+}
 
 profileButtonEdit.addEventListener("click", function () {
   popUp.classList.add("popup_opened");
