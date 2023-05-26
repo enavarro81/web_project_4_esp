4f2d6517fa8360cd503db745c5d4f8afb8c8bcc9
Esteban Navarro <esteban.navarro.orellana@gmail.com>
2023-05-25 21:33:14 -0500

    Feature: Funciones javascipt Eliminar y like

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,15 @@
 const profileName = document.querySelector(".profile__name");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 const profileButtonEdit = document.querySelector(".profile__button-edit");
+const profileButtonAdd = document.querySelector(".profile__button-add");
 
 const popUp = document.querySelector(".popup");
 const popUpButtonClose = document.querySelector(".popup__button-close");
 
 const popUpForm = document.querySelector(".popup__form");
-const popUpName = document.querySelector(".popup__input_name");
-const popUpAboutUs = document.querySelector(".popup__input_about-us");
+const popUpTittle = document.querySelector(".popup__title");
+const popUpInput1 = document.querySelector(".popup__input_1");
+const popUpInput2 = document.querySelector(".popup__input_2");
 const popUpButtonSave = document.querySelector(".popup__button-save");
 
 const initialCards = [
@@ -41,11 +43,10 @@ initialCards.forEach((element) => {
   addElement(element.name, element.link);
 });
 
-function addElement(name, link) {
+function addElement(name, link, flag = true) {
   const elements = document.querySelector(".elements");
   const elementTemplate = document.querySelector("#element-template").content;
   const elementItem = elementTemplate.querySelector(".element").cloneNode(true);
-  const elementImage = elementItem.querySelector(".element__image");
 
   elementItem.querySelector(".element__title").textContent = name;
 
@@ -53,13 +54,37 @@ function addElement(name, link) {
     .querySelector(".element__image")
     .setAttribute("style", "background-image: url(" + link + ")");
 
-  elements.append(elementItem);
+  elementItem
+    .querySelector(".element__like")
+    .addEventListener("click", function (event) {
+      event.target.classList.toggle("element__like_theme_active");
+    });
+
+  elementItem
+    .querySelector(".element__trash")
+    .addEventListener("click", function (event) {
+      elementItem.remove();
+    });
+
+  flag === true ? elements.append(elementItem) : elements.prepend(elementItem);
 }
 
 profileButtonEdit.addEventListener("click", function () {
   popUp.classList.add("popup_opened");
-  popUpName.value = profileName.textContent;
-  popUpAboutUs.value = profileSubtitle.textContent;
+  popUpTittle.textContent = "Editar perfil";
+  popUpInput1.value = profileName.textContent;
+  popUpInput1.setAttribute("placeholder", "Nombre");
+  popUpInput2.value = profileSubtitle.textContent;
+  popUpInput2.setAttribute("placeholder", "Acerca de mi");
+});
+
+profileButtonAdd.addEventListener("click", function () {
+  popUp.classList.add("popup_opened");
+  popUpTittle.textContent = "Nuevo lugar";
+  popUpInput1.value = "";
+  popUpInput1.setAttribute("placeholder", "Título");
+  popUpInput2.value = "";
+  popUpInput2.setAttribute("placeholder", "Enlace a la imagen");
 });
 
 popUpButtonClose.addEventListener("click", function () {
@@ -68,10 +93,16 @@ popUpButtonClose.addEventListener("click", function () {
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  profileName.innerHTML = popUpName.value;
-  profileSubtitle.innerHTML = popUpAboutUs.value;
-  popUpName.value = "";
-  popUpAboutUs.value = "";
+
+  if (popUpTittle.textContent === "Editar perfil") {
+    profileName.innerHTML = popUpInput1.value;
+    profileSubtitle.innerHTML = popUpInput2.value;
+  } else {
+    addElement(popUpInput1.value, popUpInput2.value, false);
+  }
+
+  popUpInput1.value = "";
+  popUpInput2.value = "";
   popUp.classList.remove("popup_opened");
 }
 
