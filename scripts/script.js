10883e54cc5a1a3c5afa2d31c29762b1d9de591c
Esteban Navarro <esteban.navarro.orellana@gmail.com>
2023-05-26 19:03:05 -0500

    Feature: Terminar efectos de transiciones al abrir y cerrar modals

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,13 @@ const profileButtonEdit = document.querySelector(".profile__button-edit");
 const profileButtonAdd = document.querySelector(".profile__button-add");
 
 const popUp = document.querySelector(".popup");
+const popUpContainer = document.querySelector(".popup__container");
+const popUpImage = document.querySelector(".popup__image");
+const popUpImageCaption = document.querySelector(".popup__image-caption");
 const popUpButtonClose = document.querySelector(".popup__button-close");
-
+const popUpButtonCloseImg = document.querySelector(
+  ".popup__button-close_theme_image"
+);
 const popUpForm = document.querySelector(".popup__form");
 const popUpTittle = document.querySelector(".popup__title");
 const popUpInput1 = document.querySelector(".popup__input_1");
@@ -55,8 +60,23 @@ function addElement(name, link, flag = true) {
     .setAttribute("style", "background-image: url(" + link + ")");
 
   elementItem
+    .querySelector(".element__image")
+    .addEventListener("click", function (event) {
+      popUp.classList.toggle("popup_theme_opened");
+      popUp.classList.remove("popup_theme_closed");
+      popUpContainer.setAttribute("style", "display: none;");
+      popUpImage.setAttribute("style", "visibility: visible;");
+      popUpImage
+        .querySelector(".popup__image-frame")
+        .setAttribute("src", event.target.style.backgroundImage.split('"')[1]);
+      popUpImageCaption.textContent =
+        event.target.parentElement.children[2].children[0].textContent;
+    });
+
+  elementItem
     .querySelector(".element__like")
     .addEventListener("click", function (event) {
+      event.target.classList.toggle("element__like_theme_inactive");
       event.target.classList.toggle("element__like_theme_active");
     });
 
@@ -69,8 +89,15 @@ function addElement(name, link, flag = true) {
   flag === true ? elements.append(elementItem) : elements.prepend(elementItem);
 }
 
+function setearOpenPopUp() {
+  popUp.classList.toggle("popup_theme_opened");
+  popUp.classList.remove("popup_theme_closed");
+  popUpContainer.setAttribute("style", "visibility: visible;");
+  popUpImage.setAttribute("style", "display: none;");
+}
+
 profileButtonEdit.addEventListener("click", function () {
-  popUp.classList.add("popup_opened");
+  setearOpenPopUp();
   popUpTittle.textContent = "Editar perfil";
   popUpInput1.value = profileName.textContent;
   popUpInput1.setAttribute("placeholder", "Nombre");
@@ -79,7 +106,7 @@ profileButtonEdit.addEventListener("click", function () {
 });
 
 profileButtonAdd.addEventListener("click", function () {
-  popUp.classList.add("popup_opened");
+  setearOpenPopUp();
   popUpTittle.textContent = "Nuevo lugar";
   popUpInput1.value = "";
   popUpInput1.setAttribute("placeholder", "Título");
@@ -87,8 +114,17 @@ profileButtonAdd.addEventListener("click", function () {
   popUpInput2.setAttribute("placeholder", "Enlace a la imagen");
 });
 
+function setearClosePopUp() {
+  popUp.classList.toggle("popup_theme_opened");
+  popUp.classList.add("popup_theme_closed");
+}
+
 popUpButtonClose.addEventListener("click", function () {
-  popUp.classList.remove("popup_opened");
+  setearClosePopUp();
+});
+
+popUpButtonCloseImg.addEventListener("click", function () {
+  setearClosePopUp();
 });
 
 function handleProfileFormSubmit(evt) {
@@ -103,7 +139,8 @@ function handleProfileFormSubmit(evt) {
 
   popUpInput1.value = "";
   popUpInput2.value = "";
-  popUp.classList.remove("popup_opened");
+  popUp.classList.toggle("popup_theme_opened");
+  popUp.classList.add("popup_theme_closed");
 }
 
 popUpForm.addEventListener("submit", handleProfileFormSubmit);
